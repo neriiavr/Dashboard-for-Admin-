9fb24cb07099ec9111aebee576aeef147153e042
neriiavr <avrupalineriman@gmail.com>
2023-01-19 19:54:37 +0100

    added color picker

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { ColorPickerComponent  } from '@syncfusion/ej2-react-inputs';
+
+import { Header } from "../components";
 
 const ColorPicker = () => {
   return (
-    <div>ColorPicker</div>
+    <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
+    <Header category="App" title="ColorPicker" />
+   
+      <div className='text-center'>
+        <div id="preview" />
+        <div className='flex justify-center items-center gap-20 flex-wrap'>
+          <div>
+            <p></p>
+          </div>
+        </div>
+      </div>
+    </div>
+
   )
 }
 
